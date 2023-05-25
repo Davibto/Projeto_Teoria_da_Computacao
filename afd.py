3d8335a426dcbd3c70456d5a90fb872551d175f5
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2023-05-25 19:34:44 -0300

    AFD valido() corrigido

--- afd.py
+++ afd.py
@@ -1,21 +1,21 @@
 class AFD:
 
-    def __init__(self, arquivo) -> None: #Cria AFD a partir de um arquivo
-        #Descricao de um AFD 5-upla = (Σ, Q, σ, q0, F)
-        self.alfabeto = [] #Σ
-        self.estados = [] #Q
-        self.transicoes = [] #σ
-        self.estado_inicial = None  #q0
-        self.finais = [] #F
+    def __init__(self, arquivo) -> None: # Cria AFD a partir de um arquivo
+        # Descricao de um AFD 5-upla = (Σ, Q, σ, q0, F)
+        self.alfabeto = [] # Σ
+        self.estados = [] # Q
+        self.transicoes = [] # σ
+        self.estado_inicial = None  # q0
+        self.finais = [] # F
         self.transicoes_tabela = [len(self.estados)][len(self.estados)] #σ
 
-        #Convertendo o arquivo .txt em afd 
+        # Convertendo o arquivo .txt em afd 
         with open(arquivo, 'r') as arquivo:
             texto = arquivo.readlines()         
         for linha in texto:
             if 'alfabeto' in linha:
                 alfabeto = linha.split(':')[1].split(',')
-                self.alfabeto = [a.strip() for a in alfabeto] #Tratamento da string para remover espacos em branco
+                self.alfabeto = [a.strip() for a in alfabeto] # Tratamento da string para remover espacos em branco
             elif 'estados' in linha:
                 estados = linha.split(':')[1].split(',')
                 self.estados = [e.strip() for e in estados]
@@ -29,18 +29,19 @@ class AFD:
             else:
                 self.transicoes.append(linha.strip())
 
-        #Criando a matriz/tabela da funcao de transições
+        # Criando a matriz/tabela da funcao de transições
         linha = len(self.estados)
         coluna = len(self.alfabeto)
-        #Atribuindo None nas transicoes, caso no final do processo a transicao nao exista, o espaco estara none
+        # Atribuindo None nas transicoes, caso no final do processo a transicao nao exista, o espaco estara none
         self.transicoes_tabela = [[None for _ in range(coluna +1)] for _ in range(linha +1)]
         for i in range(linha):
             self.transicoes_tabela[i+1][0] = self.estados[i]
         for i in range(coluna):
             self.transicoes_tabela[0][i+1] = self.alfabeto[i]
 
-    def valido(self): #Verifica se o AFD e valido
-        if self.estado_inicial == '': #Se nao tem estado inicial, nao e AFD
+    def valido(self): # Verifica se o AFD e valido
+        if self.estado_inicial == '': # Se nao tem estado inicial, nao e AFD
+            print('Se nao tem estado inicial, nao e AFD.')
             return False
         else:
             linha = len(self.estados)
@@ -52,22 +53,26 @@ class AFD:
                 s = transicao.split(',')[2]
 
                 if (qi not in self.estados) or (qf not in self.estados) or (s not in self.alfabeto):
-                    return False #Estado nao pertence ao AFD ou simbolo da transicao nao pertence ao alfabeto compreendido pelo AFD
+                    print('Um ou mais estados na lista de transicao nao pertence ao AFD ou simbolo da transicao nao pertence ao alfabeto compreendido pelo AFD.')
+                    return False # Estado nao pertence ao AFD ou simbolo da transicao nao pertence ao alfabeto compreendido pelo AFD
                 
-                for i in range(linha):#Todo estado tem uma transicao para cada simbolo
+                for i in range(linha): # Todo estado tem uma transicao para cada simbolo
                     if self.transicoes_tabela[i+1][0] == qi:
                         for j in range(coluna):
-                            if self.transicoes_tabela[i+1][j+1] != None: #Tem mais de uma transicao com o mesmo simbolo saindo do estado
-                                return False
-                            elif self.transicoes_tabela[0][j+1] == s:
-                                self.transicoes_tabela[i+1][j+1] = qf
+                            if self.transicoes_tabela[0][j+1] == s:
+                                if self.transicoes_tabela[i+1][j+1] != None: # Tem mais de uma transicao com o mesmo simbolo saindo do estado
+                                    print(self.transicoes_tabela[i+1][j+1]);
+                                    print('Tem mais de uma transicao com o mesmo simbolo saindo do estado.')
+                                    return False
+                                elif self.transicoes_tabela[0][j+1] == s:
+                                    self.transicoes_tabela[i+1][j+1] = qf
                                 break
                         break
 
  
         return self.verificar_elementos_matriz(self.transicoes_tabela)
             
-    def verificar_elementos_matriz(self, matriz): #Retorna False caso haja mais de um elemento None na tabela de transicoes
+    def verificar_elementos_matriz(self, matriz): # Retorna False caso haja mais de um elemento None na tabela de transicoes
         contador = 0
         for linha in matriz:
             for elemento in linha:
--- main.py
+++ main.py
@@ -1,56 +1,68 @@
 from afd import AFD
 
-# Definino o arquivo que sera lido
-#arquivo = "AFDteste.txt"
+### Criando o AFD a partir de um .txt
+## 1.Definino o arquivo que sera lido
+# Arquivo = "AFDteste.txt"
 arquivo = "C:/Users/arthu/Vscode/Python/MinimizadorAFD/Projeto_Teoria_da_Computacao/AFDcerto.txt"
 
-# Criando AFD
-afd = AFD(arquivo) 
+## 2.Criando o AFD
+afd = AFD(arquivo)
+## 3.Validando o AFD
+afd_valido = afd.valido()
 
-# Teste da leitura do arquivo
-"""print("Alfabeto:")
-print(afd.alfabeto)
-print("Estados:")
-print(afd.estados)
-print("Estado inicial:")
-print("['" + afd.estado_inicial + "']")
-print("Estados finais:")
-print(afd.finais)
-print("Transicoes:")
-print(afd.transicoes)
-print("Tabela de transicoes:")
-print(afd.valido())
-print(afd.transicoes_tabela);"""
+if afd_valido == False:
+    print('Nao foi possivel gerar um AFD a partir do arquivo dado.')
+else:
+    # Teste da leitura do arquivo
+    """print("Alfabeto:")
+    print(afd.alfabeto)
+    print("Estados:")
+    print(afd.estados)
+    print("Estado inicial:")
+    print("['" + afd.estado_inicial + "']")
+    print("Estados finais:")
+    print(afd.finais)
+    print("Transicoes:")
+    print(afd.transicoes)
+    print("Tabela de transicoes:")
+    print(afd.valido())
+    print(afd.transicoes_tabela);"""
 
-# Preenchendo a tabela de transicoes Verificando as transicoe
+    ### Implementando o metodo de Myhill Nerode
+    ## 1. Criando os pares de todos os estados pertencentes ao AFD
+    conjunto_estados = set(afd.estados)
+    conjunto_estados_finais = set(afd.finais)
+    # Estados nao finais e a diferenca entre os conjunto de estados e conjunto de estados finais
+    estados_nao_finais = list(conjunto_estados - conjunto_estados_finais)
 
-#Criando os pares de estados
-pares_marcados = []
-pares_nao_marcados = []
-conjunto1 = set(afd.estados)
-conjunto2 = set(afd.finais)
+    pares_marcados = []
+    pares_nao_marcados = []
 
-estados_nao_finais = list(conjunto1 - conjunto2) #cria os estados nao finais a partir da diferenca entre os estados e os estados finais
-
-for i in range(len(estados_nao_finais)): #cria os pares de estados nao finais
-    for j in range(i+1, len(estados_nao_finais)):
-        if estados_nao_finais[i] != estados_nao_finais[j]:
-            pares_nao_marcados.append([estados_nao_finais[i], estados_nao_finais[j]])
-        
-for i in range(len(afd.finais)): #cria os pares de estados finais
-    for j in range(i+1, len(afd.finais)):
-        if afd.finais[i] != afd.finais[j]:
-            pares_nao_marcados.append([afd.finais[i], afd.finais[j]])
-
-for i in range(len(estados_nao_finais)): #cria os pares de estados marcados
-    for j in range(len(afd.finais)):
-        if estados_nao_finais[i] != afd.finais[j]:
-            pares_marcados.append([estados_nao_finais[i], afd.finais[j]])
+    for i in range(len(estados_nao_finais)): # Cria os pares de estados nao finais
+        for j in range(i+1, len(estados_nao_finais)):
+            if estados_nao_finais[i] != estados_nao_finais[j]:
+                pares_nao_marcados.append([estados_nao_finais[i], estados_nao_finais[j]])
             
-print("Pares nao marcados (possiveis equivalentes):")
-print(pares_nao_marcados)
-print("Pares marcados (nao equivalentes):")
-print(pares_marcados)
+    for i in range(len(afd.finais)): # Cria os pares de estados finais
+        for j in range(i+1, len(afd.finais)):
+            if afd.finais[i] != afd.finais[j]:
+                pares_nao_marcados.append([afd.finais[i], afd.finais[j]])
 
-#STEP 3
+    for i in range(len(estados_nao_finais)): # Cria os pares de estados marcados
+        for j in range(len(afd.finais)):
+            if estados_nao_finais[i] != afd.finais[j]:
+                pares_marcados.append([estados_nao_finais[i], afd.finais[j]])
+                
+    print("Pares nao marcados (possiveis equivalentes):")
+    print(pares_nao_marcados)
+    print("Pares marcados (nao equivalentes):")
+    print(pares_marcados)
 
+    ### STEP 3 if !alterou: end
+    # while (!alterou) inicialmente False
+    # for percorrer pares_nao_marcados, pega string[0] e string[1], buscar o resultado da transicao na tabela de transicao
+    # if resultado da transicao in pares_marcados: pares_marcados.append(string) -> i = 0
+    # else:
+    #  alterou = True
+
+    ### STEP 4 Combine all the unmarked pairs and make them a single state in the minimized DFA.
