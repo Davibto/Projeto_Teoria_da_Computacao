f28afde7a69e7ec7062382ed357a406636d0c4c9
Arthur Ramon Souza <arthuramon.souza93@hotmail.com>
2023-05-27 01:12:59 -0300

    Minimizar passo 2 concluido Co-authored-by: Davi Baratto <Davibto@users.noreply.github.com>

--- minimizador.py
+++ minimizador.py
@@ -6,11 +6,10 @@ class MinimizadorAFD:
     def minimizar(afd):
         ### Implementando o metodo de Myhill Nerode
         ## 1. Criando os pares de todos os estados pertencentes ao AFD
-        conjunto_estados = set(afd.estados)
-        conjunto_estados_finais = set(afd.finais)
-        # Estados nao finais e a diferenca entre os conjunto de estados e conjunto de estados finais
-        estados_nao_finais = list(conjunto_estados - conjunto_estados_finais)
-
+        conjunto_estados = afd.estados
+        print('conjuto estados: ', conjunto_estados)
+        conjunto_estados_finais = afd.finais
+        estados_nao_finais = [estado for estado in conjunto_estados if estado not in conjunto_estados_finais] # Estados nao finais eh a diferenca entre os estados e os estados finais
         pares_marcados = []
         pares_nao_marcados = []
 
@@ -35,48 +34,54 @@ class MinimizadorAFD:
         print(pares_marcados)
 
         ### 2.Verificar as transicoe dos pares de estados nao marcados, ate que nao seja possivel marcar mais nenhum par
-        alterou = True;
-        while (alterou): # Enquanto houverem alteracoes em pares_nao_marcados e em pares_marcados
-            for par in pares_nao_marcados:
+        alterou = True
+        while(alterou): # Enquanto houver alteracoes em pares_nao_marcados e pares_marcados, verifica os estados de pares_nao_marcados
+            count = 0
+            while(count < len(pares_nao_marcados)): # Percorrer o array a cada alteracao
+                print('count: ', count, 'len: ', len(pares_nao_marcados))
+                par = pares_nao_marcados[count]
                 qi = par[0]
                 qf = par[1]
-                print('Par: ' + qi, qf)
                 linha = len(afd.estados)
                 coluna = len(afd.alfabeto)
                 estado_resultado_qi = None
                 estado_resultado_qf = None
-                
+                #print(qi, qf) ta pegando todos os estados
+
                 for s in afd.alfabeto: # Verificando os estados resultantes das transicoes
-                    for i in range(linha): # Coluna referente ao qi na tabela de transicoes
+                    for i in range(linha): # Linha referente ao qi na tabela de transicoes
                         if afd.transicoes_tabela[i+1][0] == qi:
-                            for j in range(coluna):
+                            for j in range(coluna): # Coluna referente ao simbolo s na tabela de transicoes
                                 if afd.transicoes_tabela[0][j+1] == s: # Pegando as transicoes s de qi
                                     estado_resultado_qi = afd.transicoes_tabela[i+1][j+1] # Achou o primeiro estado resultante da transicao s
                                     break
                             break
-                    
-                    for i in range(linha): # Pegando o resultado da transicao s do segundo estado
+                    for i in range(linha): # Linha referente ao qf na tabela de transicoes
                         if afd.transicoes_tabela[i+1][0] == qf:
-                            for j in range(coluna):
-                                if afd.transicoes_tabela[0][j+1] == s: 
+                            for j in range(coluna): # Coluna referente ao simbolo s na tabela de transicoes
+                                if afd.transicoes_tabela[0][j+1] == s:  # Pegando as transicoes s de qi
                                     estado_resultado_qf = afd.transicoes_tabela[i+1][j+1] # Achou o segundo estado resultante da transicao s
-                                    
-                                    print('Resultado transicao ' + s + ':' + estado_resultado_qi, estado_resultado_qf)
-                                    par_resultante = [] # Array para armazenar o resultado da transicao na tabela de transicao
-                                    par_resultante.append(estado_resultado_qi)
-                                    par_resultante.append(estado_resultado_qf)
-                                    
-                                    if (par_resultante in pares_marcados) and (par in pares_nao_marcados):
-                                        print('Par resultante: ' + par_resultante[0] + par_resultante[1] + ' esta marcado')
-                                        print(par)
-                                        pares_nao_marcados.remove(par)
-                                        pares_marcados.append(par)
-                                        MinimizadorAFD.minimizar(afd)
-                                        break
-                                    else:
-                                        alterou = False
-                                        break
+                                    break
                             break
+                    break
+                
+                par_resultante = [] # Array para armazenar o resultado da transicao na tabela de transicao
+                par_resultante.append(estado_resultado_qi)
+                par_resultante.append(estado_resultado_qf)
+
+                if (par_resultante in pares_marcados):
+                    print('Par resultante: ' + par_resultante[0] + par_resultante[1] + ' esta marcado')
+                    print(par)
+                    pares_nao_marcados.remove(par)
+                    pares_marcados.append(par)
+                    count = 0
+                else:
+                    count +=1
+                    if count == len(pares_nao_marcados)-1: 
+                        alterou = False
+            #while(i < len(pares_nao_marcados))                     
+
+            break
 
         print("Pares nao marcados (possiveis equivalentes):")
         print(pares_nao_marcados)
